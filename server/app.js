ccfd7ad5375e6c39e06d30c9571a7e7baa91b9c6
Yash Agrahari <yashagrahari456@gmail.com>
2024-07-07 00:03:37 +0530

    Switched to ES Modules

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/db');
+import express from 'express';
+import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -8,4 +9,14 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
-module.exports = app;
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
+export default app;
